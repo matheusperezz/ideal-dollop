8fd0bfb9561c129683c339cb5190b293799f9279
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 <matheus.mperez08@gmail.com>
2023-05-17 22:45:13 -0300

    cars and user models added

---
+++ src/models/carModel.ts
@@ -0,0 +1,12 @@
+class Car {
+    make: string;
+    model: string;
+    year: number;
+
+
+    constructor(make: string, model: string, year: number){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+}
